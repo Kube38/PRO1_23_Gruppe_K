d7f631944a63d328efad9fdffe7f0896494d42e6
Kube38 <119731057+Kube38@users.noreply.github.com>
2023-10-31 13:29:08 +0100

    Update der Abgabe zu Ü0
    Aufgabe (a):
    Eine Funktion, welche die Abweichung der originalen Note auf Basis der Subjektivität des Benotenden berechnet.
    
    So sind Bedingungen, die den Benotenden in seinem Urteil beeinflussen, hier aufgeführt und fließen in die Benotung ein.
    
    Positiv heißt, dass Punkte abgezogen werden.
    (Je niedriger die Note, desto besser)
    Negativ führt zur Addierung von Notenpunkten.
    
    Sonniges Wetter wirkt sich positiv auf die Benotung aus, regnerisches Wetter negativ und stürmisches sehr negativ. Wolkig wird nicht berücksichtigt, da dieses Wetter keinen Einfluss hat.
    
    Das Betriebssystem Linux wirkt sich sehr positiv, Windows negativ und Mac sehr negativ auf die Note aus.
    
    Aufgabe (b):
    
    Erst werden Eingaben des Wetters und Betriebssystem erfragt und gespeichert.
    Dann wird die CSV Datei im read-Modus ('r') geöffnet, um die darin enthaltenen Dateien zu verarbeiten.
    Der csv_reader wird verwendet, um die Zeilen der Datei zu lesen, wobei die erste Zeile mit next(csv_reader) übersprungen wird.
    Eine for Schleife wandelt erst jede Note in einen float um, um damit weiterarbeiten zu können und ersetzt sie dann durch die neue Note, basierend auf Wetter und BS,
    
    Diese try-except-Blöcke dienen der Fehlerbehandlung. Wenn die angegebene CSV-Datei nicht gefunden wird, wird eine entsprechende Meldung ausgegeben. Bei anderen Fehlern wird eine allgemeine Fehlermeldung (gespeichert in e) angezeigt, um auf Probleme hinzuweisen.
    
    Aufgabenteil (c):
    
    Hier wird mithilfe von random die Note nicht mehr aus der csv Datei übernommen, sondern zufällig aus den Zahlen 1-5 generiert. Die Einflüsse durch Wetter und BS bleiben jedoch gleich.
    
    Die neu generierte Note wird in eine neue Datei geschrieben.

--- noten_wuerfeln.py
+++ noten_wuerfeln.py
@@ -1,3 +1,6 @@
+
+# Aufgabenteil (a)
+
 def berechne_note(wetter, betriebssystem, note):
     #Wetterbedingungen als Einfluss
     if wetter == "sonnig":
@@ -16,3 +19,65 @@ def berechne_note(wetter, betriebssystem, note):
         note =+ 1.0
 
     return note
+
+#________________________________________________________________
+#Aufgabenteil (b)
+
+import csv
+
+wetter = input("Wie ist das Wetter heute so? (Auswahl zwischen: sonnig, wolkig, regnerisch, stürmisch): ")
+csv_datei = input("Bitte geben Sie den Dateinamen der CSV-Datei ein: ")
+
+try:
+    with open(csv_datei, 'r') as file:
+        csv_reader = csv.reader(file)
+        header = next(csv_reader)
+
+        for row in csv_reader:
+            name, note, bs = row
+            note = float(note)
+            neue_note = berechne_note(wetter, bs, note)
+            print(f"{name}: Neue Note - {neue_note:.1f}")
+
+except FileNotFoundError:
+    print(f"Die Datei {csv_datei} wurde nicht gefunden, überprüfe nochmal deine Eingabe.")
+except Exception as e:
+    print(f"Ein Fehler ist aufgetreten: {str(e)}")
+
+#________________________________________________________________________
+#Aufgabenteil(c)
+#Die Abgabe der Studierenden an sich zu benoten ist eigentlich auch viel zu viel Arbeit.
+#Streicht die ursprüngliche Note aus der csv Datei und schreibt das Programm so um,
+#dass die ursprüngliche Note einfach gewürfelt wird.  Das Wetter und das
+#Betriebssystem haben weiterhin die gleiche Auswirkung wie bisher.
+
+
+import csv
+import random
+
+wetter = input("Wie ist das Wetter heute so? (Auswahl zwischen: sonnig, wolkig, regnerisch, stürmisch): ")
+csv_datei = input("Bitte geben Sie den Dateinamen der CSV-Datei ein: ")
+output_csv_datei = "result_" + csv_datei
+
+try:
+    with open(csv_datei, 'r') as input_datei, open(output_csv_datei, 'w', newline='') as output_datei:
+        csv_reader = csv.reader(input_datei)
+        header = next(csv_reader)
+
+        rows = []
+        csv_writer = csv.writer(output_datei)
+        csv_writer.writerow(header)
+
+        for row in csv_reader:
+            name, _, bs = row  # Die eigentliche Note wird nicht aus der CSV-Datei gelesen
+            ursprungs_note = random.uniform(1, 5)
+            result_note = berechne_note(wetter, bs, ursprungs_note)
+            csv_writer.writerow([name, result_note, bs])
+            rows.append([name, result_note, bs])
+
+    print(f"Die Ergebnisse wurden in die Datei '{output_csv_datei}' geschrieben.")
+
+except FileNotFoundError:
+    print(f"Die Datei {csv_datei} wurde nicht gefunden, check deine Eingabe nochmal.")
+except Exception as e:
+    print(f"Ein Fehler ist aufgetreten: {str(e)}")
